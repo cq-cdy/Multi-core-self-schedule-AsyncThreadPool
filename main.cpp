8fe6f8c204b53a0bd985e4865e7047f7a160bf83
cdy <taloncdy666@163.com>
2023-09-13 16:55:08 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
-//
-// Created by 10447 on 2023-09-05.
-//
 
 #include "threadpool.h"
 
-using uLong = unsigned  long long;
-uLong run(int b ,int e)  {
+using uLong = unsigned long long;
+
+uLong run(int b, int e) {
+    for (int j = 0; j < 15; j++) {
         uLong temp = 0;
         for (uLong i = b; i <= e; ++i) {
             temp += i;
         }
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        return temp;
     }
-int main(){
+    return 0;
+}
+
+int main() {
     ThreadPool p;
     p.setMode(PoolMode::MODE_CACHED);
-    p.start(2);
-    auto res = p.submitTask(run,1,100000000);
-    std::cout <<  res .get() <<std:: endl;
+    p.start(1);
+    p.setThreadSizeThreshHold(1);
+    auto r = p.submitTask({8, SCHED_FIFO, 99}, run, 1, 100000000);
+    std::cout << r.get() << std::endl;
     return 0;
 }
--- threadpool.h
+++ threadpool.h
@@ -1,52 +1,61 @@
 #ifndef THREADPOOL_H
 #define THREADPOOL_H
 
-#include <iostream>
-#include <utility>
-#include <vector>
-#include <queue>
-#include <memory>
+#include <sched.h>
 #include <atomic>
-#include <mutex>
 #include <condition_variable>
 #include <functional>
-#include <unordered_map>
-#include <thread>
 #include <future>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
+auto lastTime = std::chrono::high_resolution_clock::now();
 const int TASK_MAX_THRESHHOLD = 1024;
 const int THREAD_MAX_THRESHHOLD = 1024;
-const int THREAD_MAX_IDLE_TIME_S = 60;
+const int THREAD_MAX_IDLE_TIME_S = 3;
+const unsigned int NUM_CORES = std::thread::hardware_concurrency();
 
+class Task;
 
-enum class PoolMode
-{
-    MODE_FIXED,  // 固定数量的线程
-    MODE_CACHED, // 线程数量可动态增长
+class Task_Config {
+public:
+    Task_Config(int num_cores = -1, int priority = SCHED_OTHER, int mode = -1)
+            : num_cores_(num_cores), priority_(priority), mode_(mode) {}
+
+    friend Task;
+    int num_cores_;
+    int priority_;
+    int mode_;
 };
 
-class Thread
-{
+enum class PoolMode {
+    MODE_FIXED,   // 固定数量的线程
+    MODE_CACHED,  // 线程数量可动态增长
+};
+cpu_set_t cpuset;
+
+class Thread {
 public:
     using ThreadFunc = std::function<void(int)>;
 
     explicit Thread(ThreadFunc func)
-            : func_(std::move(func))
-            , threadId_(generateId_++)
-    {}
+            : func_(std::move(func)), threadId_(generateId_++) {}
+
     ~Thread() = default;
 
-    void start()
-    {
+    void launch() {
         std::thread t(func_, threadId_);
         t.detach();
     }
 
-    // 获取线程id
-    [[nodiscard]] int getId()const
-    {
-        return threadId_;
-    }
+    [[nodiscard]] int getId() const { return threadId_; }
+
 private:
     ThreadFunc func_;
     static int generateId_;
@@ -55,58 +64,47 @@ private:
 
 int Thread::generateId_ = 0;
 
-class ThreadPool
-{
+class ThreadPool {
 public:
     ThreadPool()
-            : initThreadSize_(0)
-            , taskSize_(0)
-            , idleThreadSize_(0)
-            , curThreadSize_(0)
-            , taskQueMaxThreshHold_(TASK_MAX_THRESHHOLD)
-            , threadSizeThreshHold_(THREAD_MAX_THRESHHOLD)
-            , poolMode_(PoolMode::MODE_FIXED)
-            , isPoolRunning_(false)
-    {}
+            : initThreadSize_(0),
+              taskSize_(0),
+              idleThreadSize_(0),
+              curThreadSize_(0),
+              taskQueMaxThreshHold_(TASK_MAX_THRESHHOLD),
+              threadSizeThreshHold_(THREAD_MAX_THRESHHOLD),
+              poolMode_(PoolMode::MODE_FIXED),
+              isPoolRunning_(false) {}
 
-    ~ThreadPool()
-    {
+    ~ThreadPool() {
         isPoolRunning_ = false;
 
         std::unique_lock<std::mutex> lock(taskQueMtx_);
         notEmpty_.notify_all();
-        exitCond_.wait(lock, [&]()->bool {return threads_.empty(); });
+        exitCond_.wait(lock, [&]() -> bool { return threads_.empty(); });
+
     }
 
-    void setMode(PoolMode mode)
-    {
-        if (checkRunningState()){
-            std::cout << " already start!\n";
+    void setMode(PoolMode mode) {
+        if (checkRunningState()) {
             return;
         }
         poolMode_ = mode;
     }
 
-    void setTaskQueMaxThreshHold(int threshhold)
-    {
-        if (checkRunningState())
-            return;
+    void setTaskQueMaxThreshHold(int threshhold) {
+        if (checkRunningState()) return;
         taskQueMaxThreshHold_ = threshhold;
     }
 
-    void setThreadSizeThreshHold(int threshhold)
-    {
-        if (checkRunningState())
-            return;
-        if (poolMode_ == PoolMode::MODE_CACHED)
-        {
+    void setThreadSizeThreshHold(int threshhold) {
+        if (checkRunningState()) return;
+        if (poolMode_ == PoolMode::MODE_CACHED) {
             threadSizeThreshHold_ = threshhold;
         }
     }
 
-
     template<typename Func, typename... Args>
-
     /**
      * packaged_task用来包函数
      * 然后 packaged_task类里面有一个 类似于Any类 的 手法，
@@ -116,37 +114,44 @@ public:
      * 还没有，就会阻塞等待，等 函数执行完 ，把函数 的返回值
      * 给到Any成员时，就会唤醒阻塞的get，并将返回值给从get返回给用户
      */
-    auto submitTask(Func&& func, Args&&... args) -> std::future<decltype(func(args...))>
-    {
+    auto submitTask(Task_Config config, Func &&func, Args &&... args)
+    -> std::future<decltype(func(args...))> {
         using RType = decltype(func(args...));
-        auto task = std::make_shared<std::packaged_task<RType()>>(
+        auto task_f = std::make_shared<std::packaged_task<RType()>>(
                 std::bind(std::forward<Func>(func), std::forward<Args>(args)...));
-        std::future<RType> result = task->get_future();
+
+        std::future<RType> result = task_f->get_future();
         std::unique_lock<std::mutex> lock(taskQueMtx_);
-        if (!notFull_.wait_for(lock, std::chrono::seconds(1),
-                               [&]()->bool { return taskQue_.size() < (size_t)taskQueMaxThreshHold_; }))
-        {
-            std::cerr << "task queue is full, submit task fail." << std::endl;
+        if (!notFull_.wait_for(lock, std::chrono::seconds(1), [&]() -> bool {
+            return taskQue_.size() < (size_t) taskQueMaxThreshHold_;
+        })) {
             auto task_ = std::make_shared<std::packaged_task<RType()>>(
-                    []()->RType { return RType(); });
+                    []() -> RType { return RType(); });
+            //todo a task submit fail tips
             (*task_)();
             return task_->get_future();
         }
-
-        taskQue_.emplace([task]() {(*task)();});
+        auto task_func = [task_f]() { (*task_f)(); };
+        taskQue_.emplace(task_func, config);
         taskSize_++;
-
         notEmpty_.notify_all();
 
-        if (poolMode_ == PoolMode::MODE_CACHED
-            && taskSize_ > idleThreadSize_
-            && curThreadSize_ < threadSizeThreshHold_)
-        {
-            std::cout << ">>> create new thread..." << std::endl;
-            auto ptr = std::make_unique<Thread>([this](auto && PH1) { threadFunc(std::forward<decltype(PH1)>(PH1)); });
+        /*  std::cout << " taskSize_ =  " << taskSize_ << " idleThreadSize_ "
+                   << idleThreadSize_ << "\n";
+         std::cout << " curThreadSize_ =  " << curThreadSize_
+                   << "  threadSizeThreshHold_ " << threadSizeThreshHold_
+                   << "\n"; */
+        if (poolMode_ == PoolMode::MODE_CACHED && taskSize_ > idleThreadSize_ &&
+
+            curThreadSize_ < threadSizeThreshHold_) {
+            /* std::cout << " ----------------------------------------------- "
+                         "create new thread...\n"; */
+            auto ptr = std::make_unique<Thread>([this](auto &&PH1) {
+                threadFunc(std::forward<decltype(PH1)>(PH1));
+            });
             int threadId = ptr->getId();
             threads_.emplace(threadId, std::move(ptr));
-            threads_[threadId]->start();
+            threads_[threadId]->launch();
             curThreadSize_++;
             idleThreadSize_++;
         }
@@ -154,32 +159,29 @@ public:
         return result;
     }
 
-    // 开启线程池
-    void start(int initThreadSize =(int)std::thread::hardware_concurrency())
-    {
+    void start(int initThreadSize = (int) std::thread::hardware_concurrency()) {
         isPoolRunning_ = true;
-
         initThreadSize_ = initThreadSize;
-        curThreadSize_ = initThreadSize;
+        idleThreadSize_ = initThreadSize_;
+        curThreadSize_ = initThreadSize_;
 
-        for (int i = 0; i < initThreadSize_; i++)
-        {
-
-            auto ptr = std::make_unique<Thread>([this](auto && PH1) { threadFunc(std::forward<decltype(PH1)>(PH1)); });
+        for (int i = 0; i < initThreadSize_; i++) {
+            auto ptr = std::make_unique<Thread>([this](auto &&PH1) {
+                threadFunc(std::forward<decltype(PH1)>(PH1));
+            });
             int threadId = ptr->getId();
             threads_.emplace(threadId, std::move(ptr));
-
         }
 
-        for (int i = 0; i < initThreadSize_; i++)
-        {
-            threads_[i]->start();
+        for (int i = 0; i < initThreadSize_; i++) {
+            threads_[i]->launch();
             idleThreadSize_++;
         }
     }
 
-    ThreadPool(const ThreadPool&) = delete;
-    ThreadPool& operator=(const ThreadPool&) = delete;
+    ThreadPool(const ThreadPool &) = delete;
+
+    ThreadPool &operator=(const ThreadPool &) = delete;
 
 private:
     /*
@@ -187,92 +189,98 @@ private:
      * 每个线程函数 执行的时候就会判断是否需要线程数缩减
      * 而是否能增加线程数，是在submitTask哪里做的。
      */
-    void threadFunc(int threadid)
-    {
-        auto lastTime = std::chrono::high_resolution_clock::now();
-
-        for (;;)
-        {
+    void threadFunc(int threadid) {
+        for (;;) {
             Task task;
             {
                 std::unique_lock<std::mutex> lock(taskQueMtx_);
-
-                std::cout << "tid:" << std::this_thread::get_id()
-                          << "try to get task ..." << std::endl;
-
-
-
-
-                // 锁 + 双重判断
-                while (taskQue_.empty())
-                {
-                    // 线程池要结束，回收线程资源
-                    if (!isPoolRunning_)
-                    {
-                        threads_.erase(threadid); // std::this_thread::getid()
-                        std::cout << "threadid:" << std::this_thread::get_id() << " exit!"
-                                  << std::endl;
+//                std::cout << "tid:" << std::this_thread::get_id()
+//                          << "try to get task ..." << std::endl;
+                while (taskQue_.empty()) {
+                    if (!isPoolRunning_) {
+                        threads_.erase(threadid);  // std::this_thread::getid()
+//                        std::cout << "threadid:" << std::this_thread::get_id()
+//                                  << " exit!" << std::endl;
                         exitCond_.notify_all();
                         return;
                     }
-
-                    if (poolMode_ == PoolMode::MODE_CACHED)
-                    {
+                    if (poolMode_ == PoolMode::MODE_CACHED) {
                         if (std::cv_status::timeout ==
-                            notEmpty_.wait_for(lock, std::chrono::seconds(1)))
-                        {
-                            auto now = std::chrono::high_resolution_clock::now();
-                            auto dur = std::chrono::duration_cast<std::chrono::seconds>(now - lastTime);
-                            if (dur.count() >= THREAD_MAX_IDLE_TIME_S
-                                && curThreadSize_ > initThreadSize_)
-                            {
-
+                            notEmpty_.wait_for(lock, std::chrono::seconds(1))) {
+                            auto now =
+                                    std::chrono::high_resolution_clock::now();
+                            auto dur = std::chrono::duration_cast<
+                                    std::chrono::seconds>(now - lastTime);
+                            if (dur.count() >= THREAD_MAX_IDLE_TIME_S &&
+                                curThreadSize_ > initThreadSize_) {
                                 threads_.erase(threadid);
                                 curThreadSize_--;
                                 idleThreadSize_--;
-
-                                std::cout << "threadid:" << std::this_thread::get_id() << " exit!"
-                                          << std::endl;
+//                                std::cout
+//                                        << "threadid:" << std::this_thread::get_id()
+//                                        << " exit!" << std::endl;
                                 return;
                             }
                         }
-                    }
-                    else
-                    {
+                    } else {
                         notEmpty_.wait(lock);
                     }
                 }
 
                 idleThreadSize_--;
-
-                std::cout << "tid:" << std::this_thread::get_id()
-                          << "get task..." << std::endl;
-
-                task = taskQue_.front();
-                taskQue_.pop();
-                taskSize_--;
-
-                if (!taskQue_.empty())
-                {
-                    notEmpty_.notify_all();
+//                std::cout << "tid:" << std::this_thread::get_id()
+//                          << "get task..." << std::endl;
+                task = std::move(taskQue_.front());
+                // 设置CPU核心
+                if (task.config_.num_cores_ < NUM_CORES &&
+                    task.config_.num_cores_ > 0) {
+                    CPU_ZERO(&cpuset);
+                    for (int i = 0; i < task.config_.num_cores_; ++i) {
+                        CPU_SET(i, &cpuset);
+                    }
+                    if (pthread_setaffinity_np(
+                            pthread_self(), sizeof(cpu_set_t), &cpuset) < 0) {
+                    }
+                }
+                // 设置优先级
+                struct sched_param param{};
+                param.sched_priority = task.config_.priority_;
+                if (pthread_setschedparam(pthread_self(), task.config_.mode_,
+                                          &param) > 0) {
+                    std::cerr << "some error \n";
+                    // todo throw exception
                 }
 
+                taskQue_.pop();
+                taskSize_--;
+                if (!taskQue_.empty()) {
+                    notEmpty_.notify_all();
+                }
                 notFull_.notify_all();
             }
-            if (task != nullptr)
-            {
-                task();
+            if (task.task_func != nullptr) {
+                task.task_func();
             }
             idleThreadSize_++;
             lastTime = std::chrono::high_resolution_clock::now();
         }
     }
-    bool checkRunningState() const
-    {
-        return isPoolRunning_;
-    }
 
-private:
+    bool checkRunningState() const { return isPoolRunning_; }
+
+    class Task {
+    public:
+        using task_f = std::function<void()>;
+
+        Task(task_f f = nullptr, Task_Config config = {})
+                : task_func(std::move(f)), config_(config) {}
+
+        task_f task_func = nullptr;
+        Task_Config config_;
+
+    private:
+    };
+
     std::unordered_map<int, std::unique_ptr<Thread>> threads_;
 
     int initThreadSize_;
@@ -280,7 +288,6 @@ private:
     std::atomic_int curThreadSize_;
     std::atomic_int idleThreadSize_;
 
-    using Task = std::function<void()>;
     std::queue<Task> taskQue_;
     std::atomic_int taskSize_;
     int taskQueMaxThreshHold_;
